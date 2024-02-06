a2877d2021fef8c6e65ec5fea105d3ef1ec02db9
Steve Calla <callasteven@gmail.com>
2024-02-06 14:24:56 -0700

    Test images in public and assets path

--- src/components/CharacterGallery.jsx
+++ src/components/CharacterGallery.jsx
@@ -1,18 +1,21 @@
 import characterData from "../data/characterData.json";
 import Character from "./Character";
+import imgTestImport from '../assets/testImageImport.png';
 
 const CharacterGallery = () => {
   const listOfChars = characterData.map((char, i) => (
     <Character {...char} key={i} />
   ));
-  return <section>{listOfChars}</section>;
+  return (
+    <>
+      <h1>Test Image</h1>
+      <p>Image from public</p>
+      <img src="./assets/imgTestPublic.png" style={{ width: "200px" }} />
+      <p>Image from import</p>
+      <img src={imgTestImport} style={{ width: "200px" }} />
+      <section>{listOfChars}</section>
+    </>
+  );
 };
 
 export default CharacterGallery;
-
-//
-{/* <div>
-  {characterData.map((character) => (
-    <Character key={character.id} name={character.name} />
-  ))}
-</div>; */}
